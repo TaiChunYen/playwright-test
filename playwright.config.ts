fa1dcd8f0497120b8d2de16a1c4e589b83ee1487
TaiChunYen <kevintai0304@gmail.com>
2025-11-23 11:08:50 +0800

    feat: generate report file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
     globalSetup: './tests/login.setup.ts',
+    reporter: [['html', { open: 'never' }]],
     use: {
       storageState: 'storageState.json',
       locale: 'zh-TW',
--- tests/user-test.spec.ts
+++ tests/user-test.spec.ts
@@ -1,5 +1,14 @@
 import { test, expect } from '@playwright/test';
 
+test.afterEach(async ({ page }, testInfo) => {
+  const screenshot = await page.screenshot();
+
+  await testInfo.attach('screenshot', {
+    body: screenshot,
+    contentType: 'image/png'
+  });
+});
+
 test('test', async ({ page }) => {
   await page.goto('https://bottleneko.app/user');
   await expect(page.locator('#main')).toContainText('void294');
